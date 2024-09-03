6041043125b44d0cd8b712bb9eb1d8946d4b93ae
Nicolas Bastos <nickbastos4@gmail.com>
2024-09-03 14:49:42 -0500

    Update index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -35,6 +35,7 @@ type Omit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
 export {
   registerPlugin,
   FileStatus,
+  FilePondFile
 } from 'filepond';
 
 export interface FilePondProps extends FilePondOptions {
